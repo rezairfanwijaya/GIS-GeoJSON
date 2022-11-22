743174198054739e9e85896469fefd1c8ca47510
Reza Irfan Wijaya <rezaaagit@gmail.com>
2022-11-22 13:26:48 +0700

    add: ajax for render geojson and add popup

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// * set map
 var map = L.map('map').setView([-7.420419566364883, 109.24936603853541], 11);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var marker = L.marker([-7.420419566364883, 109.24936603853541]).addTo(map);
 
-var geoJsonLayer = new L.GeoJSON.AJAX('banyumas.geojson')
+// * add popup
+function popupKu(f,l) {
+    var out = [];
+    if (f.properties){
+        for(key in f.properties){
+            out.push(key + ": " + f.properties[key])
+        }
+        l.bindPopup(out.join("<br>"))
+    }
+}
+
+// * render geojson
+var geoJsonLayer = new L.GeoJSON.AJAX('banyumas.geojson', {onEachFeature:popupKu})
 geoJsonLayer.addTo(map)
